d390b795c3ff62856ca5f957bc71140cb38027a0
Filip Ornstein <filip@sinfin.cz>
2015-06-25 20:04:09 +0200

    + stmp config

---
+++ templates/skeleton/config/initializers/smtp.rb
@@ -0,0 +1,14 @@
+if Rails.env.production?
+  SMTP_SETTINGS = {
+    address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
+    authentication: :plain,
+    domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
+    enable_starttls_auto: true,
+    password: ENV.fetch("SMTP_PASSWORD"),
+    port: "587",
+    user_name: ENV.fetch("SMTP_USERNAME")
+  }
+
+  Rails.application.config.action_mailer.delivery_method = :smtp
+  Rails.application.config.action_mailer.smtp_settings = SMTP_SETTINGS
+end
